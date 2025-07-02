8e6ccb7b614194733339800033d2f1a9d73dc779
dahyoji <gkdud8429@naver.com>
2025-07-02 16:17:57 +0900

    Add files via upload

---
+++ js/custom.js
@@ -0,0 +1,55 @@
+$(function () {
+
+    const tl = gsap.timeline();
+
+    tl
+        .from({}, {})
+        .from('.intro h2', { y: 100, opacity: 0 })
+        .from('.intro p', { y: 100, opacity: 0 })
+        .from('.intro span', { width: 0, duration: 2 })
+        .from('.intro em', { opacity: 0 })
+
+
+    // gsap.to('.intro h2', { y: 100, duration: 2 })
+    // gsap.from('.intro h2', { y: 100, })
+    // gsap.from('.intro p', { y: 100, delay: 1 })
+
+
+    $('.wrapper').fullpage({
+        anchors: ['intro', 'portfolio01', 'portfolio02', 'portfolio03', 'portfolio04', 'portfolio05', 'training', 'profile'],
+        fixedElements: '#header, #footer',
+
+        afterLoad: function (_, idx) {
+            $('#header .gnb>ul>li').removeClass('on');
+            $('#header .gnb>ul>li').eq(idx - 1).addClass('on');
+
+            $('.section').removeClass('on');
+            $('.section').eq(idx - 1).addClass('on');
+
+        },
+
+
+        onLeave: function (_, idx, d) {
+
+            // 첫 화면에 왔을 때 애니메이션 다시 재생하기
+
+            if (idx == 1) {
+                tl.restart();
+            }
+        }
+    });
+
+    $('#header .cover_btn').on('click', function () {
+        $(this).toggleClass('on');
+        $('#header .cover').toggleClass('on');
+    })
+
+    $('#header .cover ul>li>a').on('click', function () {
+        $('#header .cover_btn').removeClass('on');
+        $('#header .cover').removeClass('on');
+    })
+    $('#header .cover').on('wheel', function (e) {
+        e.stopPropagation();
+
+    });
+});
